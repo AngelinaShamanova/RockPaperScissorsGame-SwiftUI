47cae6dc306382228ee9e75b97290819620265ae
AngelinaShamanova <shamanovaaaa@yandex.ru>
2022-11-21 23:11:03 +0100

    Initial Commit

---
+++ RockPaperScissorsGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissorsGame
+//
+//  Created by Ангелина Шаманова on 21.11.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by Ангелина Шаманова on 21.11.22..
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
